327deedf1f1a678ffe67cbdce978a5c40a749bc2
zhupeng <zhupeng@zlg.cn>
2019-07-25 15:08:44 +0800

    更正BKP底层驱动

--- soc/zlg/drivers/include/bkp/hw/amhw_zlg237_bkp.h
+++ soc/zlg/drivers/include/bkp/hw/amhw_zlg237_bkp.h
@@ -95,13 +95,17 @@ typedef struct amhw_zlg237_bkp {
     __IO uint16_t cr;        /**< \brief BKP 控制寄存器 */
     __I  uint16_t reserve2;  /**< \brief 保留 */
     __IO uint16_t csr;       /**< \brief 时钟监控状态寄存器 */
+    __I  uint16_t reserve3;  /**< \brief 保留 */
+    __I  uint32_t reserve4;  /**< \brief 保留 */
+    __IO uint32_t lse_cfg;   /**< \brief LSE晶振控制寄存器 */
 } amhw_zlg237_bkp_t;
 
 /**
  * \brief Clears Tamper Pin Event pending flag.
  *
  * \param[in] p_hw_bkp Pointer to amhw_zlg237_bkp_t Structure
- * \param[in] flag     see AMHW_BKP_CSR_CTE_SET or AMHW_BKP_CSR_CTI_SET
+ * \param[in] flag     see AMHW_ZLG237_BKP_CSR_CTE_SET or
+ *                         AMHW_ZLG237_BKP_CSR_CTI_SET
  *
  * \return None
  */
@@ -278,6 +282,156 @@ void amhw_zlg237_bkp_rtccr_asos_alarm (amhw_zlg237_bkp_t *p_hw_bkp)
     p_hw_bkp->rtc_cr &= ~AMHW_ZLG237_BKP_RTCCR_ASOS_SET;
 }
 
+/**
+ * \brief LSE 晶振IO驱动能力配置
+ */
+typedef enum amhw_zlg237_bkp_iop {
+    AMHW_ZLG237_BKP_LSECFG_IOP_0 = 0,  /*档位0  最低档位驱动能力 */
+    AMHW_ZLG237_BKP_LSECFG_IOP_1,      /*档位1 */
+    AMHW_ZLG237_BKP_LSECFG_IOP_2,      /*档位2 */
+    AMHW_ZLG237_BKP_LSECFG_IOP_3,      /*档位3  最高档位驱动能力 */
+} amhw_zlg237_bkp_iop_t;
+/**
+ * \brief IOP LSE 晶振 IO驱动能力配置
+ *
+ * \param[in] p_hw_bkp 指向系统配置寄存器块的指针
+ * \param[in] flag     传入参数应为  amhw_zlg237_bkp_iop_t 枚举中的变量
+ *
+ * \return 返回对应备份寄存器的值
+ */
+am_static_inline
+void amhw_zlg237_bkp_lsecfg_iop_set (amhw_zlg237_bkp_t     *p_hw_bkp,
+                                     amhw_zlg237_bkp_iop_t  flag)
+{
+    p_hw_bkp->lse_cfg = (p_hw_bkp->lse_cfg & (~(0x3u << 0))) | flag;
+}
+
+/**
+ * \brief IOP_MON 获取IO驱动能力当前档位
+ *
+ * \param[in] p_hw_bkp 指向系统配置寄存器块的指针
+ * \param[in] flag     传入参数应为  amhw_zlg237_bkp_iop_t 枚举中的变量
+ *
+ * \note : 当AUTO_IOP比特设置为1时，IOP[1:0]的设置值不会立即生效；LSE的驱动档位
+ *         会由2’b11逐渐降为IOP[1:0]的设置值。在此期间，可通过读取IOP_MON[1:0]
+ *         获得当前LSE的驱动档位值。
+ *
+ * \return 返回对应备份寄存器的值
+ */
+am_static_inline
+amhw_zlg237_bkp_iop_t amhw_zlg237_bkp_lsecfg_iop_get(
+    amhw_zlg237_bkp_t *p_hw_bkp)
+{
+    return (p_hw_bkp->lse_cfg & (0x3u << 3));
+}
+
+/**
+ * \brief NFBYP LSE片内模拟滤噪电器bypass使能
+ *
+ * \param[in] p_hw_bkp 指向系统配置寄存器块的指针
+ *
+ * \return none
+ */
+am_static_inline
+void amhw_zlg237_bkp_lsecfg_nfbyp_enable (amhw_zlg237_bkp_t *p_hw_bkp)
+{
+    p_hw_bkp->lse_cfg |= (1u << 6);
+}
+
+/**
+ * \brief NFBYP LSE片内模拟滤噪电器bypass禁能
+ *
+ * \param[in] p_hw_bkp 指向系统配置寄存器块的指针
+ *
+ * \return none
+ */
+am_static_inline
+void amhw_zlg237_bkp_lsecfg_nfbyp_disable (amhw_zlg237_bkp_t *p_hw_bkp)
+{
+    p_hw_bkp->lse_cfg &= ~(1u << 6);
+}
+
+/**
+ * \brief AUTO_IOP IOP设置立即生效使能
+ *
+ * \param[in] p_hw_bkp 指向系统配置寄存器块的指针
+ *
+ * \note :    0 : IOP[1:0]的LSE驱动档位设置值立即生效。
+ *            1 : LSE驱动档位将由2’b11逐渐降为IOP[1:0]的设置值。
+ *
+ * \return none
+ */
+am_static_inline
+void amhw_zlg237_bkp_lsecfg_autoiop_enable (amhw_zlg237_bkp_t *p_hw_bkp)
+{
+    p_hw_bkp->lse_cfg |= (1u << 7);
+}
+
+/**
+ * \brief AUTO_IOP IOP设置立即生效禁能
+ *
+ * \param[in] p_hw_bkp 指向系统配置寄存器块的指针
+ *
+ * \note :    0 : IOP[1:0]的LSE驱动档位设置值立即生效。
+ *            1 : LSE驱动档位将由2’b11逐渐降为IOP[1:0]的设置值。
+ *
+ * \return none
+ */
+am_static_inline
+void amhw_zlg237_bkp_lsecfg_autoiop_disable (amhw_zlg237_bkp_t *p_hw_bkp)
+{
+    p_hw_bkp->lse_cfg &= ~(1u << 7);
+}
+
+/**
+ * \brief AGC 自动增益幅度设置
+ */
+typedef enum amhw_zlg237_bkp_agc {
+    AMHW_ZLG237_BKP_LSECFG_AGC_0 = 0,  /*档位0  最低档 */
+    AMHW_ZLG237_BKP_LSECFG_AGC_1,      /*档位1 */
+    AMHW_ZLG237_BKP_LSECFG_AGC_2,      /*档位2 */
+    AMHW_ZLG237_BKP_LSECFG_AGC_3,      /*档位3  最高档 */
+} amhw_zlg237_bkp_agc_t;
+/**
+ * \brief SEL_AGC AGC 自动增益幅度设置
+ *
+ * \param[in] p_hw_bkp 指向系统配置寄存器块的指针
+ * \param[in] flag     传入参数应为  amhw_zlg237_bkp_agc_t 枚举中的变量
+ *
+ * \return 返回对应备份寄存器的值
+ */
+am_static_inline
+void amhw_zlg237_bkp_lsecfg_agc_set (amhw_zlg237_bkp_t     *p_hw_bkp,
+                                     amhw_zlg237_bkp_agc_t  flag)
+{
+    p_hw_bkp->lse_cfg = (p_hw_bkp->lse_cfg & (~(0x3u << 8))) | flag;
+}
+
+/**
+ * \brief AGC_EN LSE晶振IO内部自动增益电路使能
+ *
+ * \param[in] p_hw_bkp 指向系统配置寄存器块的指针
+ *
+ * \return none
+ */
+am_static_inline
+void amhw_zlg237_bkp_lsecfg_agc_enable (amhw_zlg237_bkp_t *p_hw_bkp)
+{
+    p_hw_bkp->lse_cfg |= (1u << 15);
+}
+
+/**
+ * \brief AGC_EN LSE晶振IO内部自动增益电路禁能
+ *
+ * \param[in] p_hw_bkp 指向系统配置寄存器块的指针
+ *
+ * \return none
+ */
+am_static_inline
+void amhw_zlg237_bkp_lsecfg_agc_disable (amhw_zlg237_bkp_t *p_hw_bkp)
+{
+    p_hw_bkp->lse_cfg &= ~(1u << 15);
+}
 /**
  * \brief 使用匿名联合体段结束
  * @{
