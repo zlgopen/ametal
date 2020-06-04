0c16ec16b526f8b65ea37337f0ab52d929f0aad8
yeruizhang <42535234+yeruizhang@users.noreply.github.com>
2020-06-04 10:59:03 +0800

    调整工程文件分组

--- examples/board/amf17x_core/miniport_board/demo_hc32f17x_core_miniport_digitron.c
+++ examples/board/amf17x_core/miniport_board/demo_hc32f17x_core_miniport_digitron.c
@@ -40,7 +40,7 @@
 #include "am_vdebug.h"
 #include "am_hc32f17x_inst_init.h"
 #include "am_digitron_disp.h"
-#include "demo_amf17x_core_entries.h"
+#include "demo_std_entries.h"
 
 /**
  * \brief Àı³ÌÈë¿Ú
