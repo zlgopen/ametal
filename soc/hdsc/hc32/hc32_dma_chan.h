9927aa5908c418dcda6668fcb94f135ba39db0cb
lichenglong <lichenglong@zlg.cn>
2020-06-18 13:59:11 +0800

    解决 hc32l1xx adc_dma例程功能异常的问题；重写hc32lx3x和hc32l19x的dma驱动不兼容部分。

--- soc/hdsc/hc32/hc32_dma_chan.h
+++ soc/hdsc/hc32/hc32_dma_chan.h
@@ -51,13 +51,50 @@ extern "C" {
  */
 
 #define HC32_DMA_SRC_TYPE_SOFT         0x00  /* 传输请求触发源为 软件 */
+
+#ifdef HC32X3X
+
+#define HC32_DMA_SRC_TYPE_SPI0_RX      0x20  /* 传输请求触发源为 SPI0_RX */
+#define HC32_DMA_SRC_TYPE_SPI0_TX      0x21  /* 传输请求触发源为 SPI0_TX */
+#define HC32_DMA_SRC_TYPE_SPI1_RX      0x22  /* 传输请求触发源为 SPI1_RX */
+#define HC32_DMA_SRC_TYPE_SPI1_TX      0x23  /* 传输请求触发源为 SPI1_TX */
+#define HC32_DMA_SRC_TYPE_ADC_JRQ0_RX  0x24  /* 传输请求触发源为 ADC_JRQ0_RX */
+#define HC32_DMA_SRC_TYPE_ADC_SRQ0_RX  0x25  /* 传输请求触发源为 ADC_SRQ0_RX */
+#define HC32_DMA_SRC_TYPE_LCD_TX       0x26  /* 传输请求触发源为 LCD_TX */
+
+#define HC32_DMA_SRC_TYPE_UART0_RX     0x28  /* 传输请求触发源为 UART0_RX */
+#define HC32_DMA_SRC_TYPE_UART0_TX     0x29  /* 传输请求触发源为 UART0_TX */
+#define HC32_DMA_SRC_TYPE_UART1_RX     0x2A  /* 传输请求触发源为 UART1_RX */
+#define HC32_DMA_SRC_TYPE_UART1_TX     0x2B  /* 传输请求触发源为 UART1_TX */
+#define HC32_DMA_SRC_TYPE_LPUART0_RX   0x2C  /* 传输请求触发源为 LPUART0_RX */
+#define HC32_DMA_SRC_TYPE_LPUART0_TX   0x2D  /* 传输请求触发源为 LPUART0_TX */
+#define HC32_DMA_SRC_TYPE_LPUART1_RX   0x2E  /* 传输请求触发源为 LPUART1_RX */
+#define HC32_DMA_SRC_TYPE_LPUART1_TX   0x2F  /* 传输请求触发源为 LPUART1_TX */
+
+#define HC32_DMA_SRC_TYPE_TIM0_CHA     0x32  /* 传输请求触发源为 TIM0_CHA */
+#define HC32_DMA_SRC_TYPE_TIM0_CHB     0x33  /* 传输请求触发源为 TIM0_CHB */
+#define HC32_DMA_SRC_TYPE_TIM1_CHA     0x34  /* 传输请求触发源为 TIM1_CHA */
+#define HC32_DMA_SRC_TYPE_TIM1_CHB     0x35  /* 传输请求触发源为 TIM1_CHB */
+#define HC32_DMA_SRC_TYPE_TIM2_CHA     0x36  /* 传输请求触发源为 TIM2_CHA */
+#define HC32_DMA_SRC_TYPE_TIM2_CHB     0x37  /* 传输请求触发源为 TIM2_CHB */
+#define HC32_DMA_SRC_TYPE_TIM3_CHA     0x38  /* 传输请求触发源为 TIM3_CHA */
+#define HC32_DMA_SRC_TYPE_TIM3_CHB     0x39  /* 传输请求触发源为 TIM3_CHB */
+#define HC32_DMA_SRC_TYPE_TIM4_CHA     0x3A  /* 传输请求触发源为 TIM4_CHA */
+#define HC32_DMA_SRC_TYPE_TIM4_CHB     0x3B  /* 传输请求触发源为 TIM4_CHB */
+#define HC32_DMA_SRC_TYPE_TIM5_CHA     0x3C  /* 传输请求触发源为 TIM5_CHA */
+#define HC32_DMA_SRC_TYPE_TIM5_CHB     0x3D  /* 传输请求触发源为 TIM5_CHB */
+#define HC32_DMA_SRC_TYPE_TIM6_CHA     0x3E  /* 传输请求触发源为 TIM6_CHA */
+#define HC32_DMA_SRC_TYPE_TIM6_CHB     0x3F  /* 传输请求触发源为 TIM6_CHB */
+
+#else
+
 #define HC32_DMA_SRC_TYPE_SPI0_RX      0x40  /* 传输请求触发源为 SPI0_RX */
 #define HC32_DMA_SRC_TYPE_SPI0_TX      0x41  /* 传输请求触发源为 SPI0_TX */
 #define HC32_DMA_SRC_TYPE_SPI1_RX      0x42  /* 传输请求触发源为 SPI1_RX */
 #define HC32_DMA_SRC_TYPE_SPI1_TX      0x43  /* 传输请求触发源为 SPI1_TX */
 #define HC32_DMA_SRC_TYPE_ADC_JRQ0_RX  0x44  /* 传输请求触发源为 ADC_JRQ0_RX */
 #define HC32_DMA_SRC_TYPE_ADC_SRQ0_RX  0x45  /* 传输请求触发源为 ADC_SRQ0_RX */
-#define HC32_DMA_SRC_TYPE_LCD_TX       0x45  /* 传输请求触发源为 LCD_TX */
+#define HC32_DMA_SRC_TYPE_LCD_TX       0x46  /* 传输请求触发源为 LCD_TX */
 
 #define HC32_DMA_SRC_TYPE_UART0_RX     0x48  /* 传输请求触发源为 UART0_RX */
 #define HC32_DMA_SRC_TYPE_UART0_TX     0x49  /* 传输请求触发源为 UART0_TX */
@@ -87,6 +124,8 @@ extern "C" {
 #define HC32_DMA_SRC_TYPE_UART4_TX     0x61  /* 传输请求触发源为 UART4_TX */
 #define HC32_DMA_SRC_TYPE_UART5_RX     0x62  /* 传输请求触发源为 UART5_RX */
 #define HC32_DMA_SRC_TYPE_UART5_TX     0x63  /* 传输请求触发源为 UART5_TX */
+
+#endif  /* HC32X3X */
 
 /** @} */
 
